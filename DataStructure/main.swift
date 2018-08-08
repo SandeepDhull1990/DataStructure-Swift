056a7579c04f7d1211773c463412d32d8f3716d1
Sandeep <sandeep.dhull@sigma.se>
2018-08-08 12:44:10 +0530

    Initial Commit

---
+++ DataStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructure
+//
+//  Created by sandeep dhull on 08/08/18.
+//  Copyright © 2018 sandeep dhull. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
